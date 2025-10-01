c413ad88d06c2171e6d13d63247282733bd5990f
Surya Raghava <surya.raghava@infomagnus.com>
2025-10-01 18:08:49 +0530

    Update S3 bucket name for Terraform backend configuration

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "my-terraform-state-surya"
+    bucket = "my-terraform-state-samir"
     key    = "s3/terraform.tfstate"
     region = "ap-south-1"
     use_lockfile = true
