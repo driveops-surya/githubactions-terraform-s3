0767fb287f0b00af6bd603718c8456be23058884
imagefactory-web <factoryimage2@gmail.com>
2026-05-19 10:15:17 +0530

    Change S3 bucket name for Terraform backend

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "my-terraform-state-samir"
+    bucket = "my-terraform-state-lokesh"
     key    = "s3/terraform.tfstate"
     region = "ap-south-1"
     use_lockfile = true
